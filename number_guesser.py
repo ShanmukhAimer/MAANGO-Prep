c79af3b0c760e7f18d22b2711c2ef0360cdc75c0
ShanmukhAimer <shanmukh250321@gmail.com>
2025-07-15 21:36:29 +0530

    Update number_guesser.py

--- number_guesser.py
+++ number_guesser.py
@@ -1,13 +1,12 @@
 import random
 
-
 def guesser():
-    randNum = random.randint(1,100)
+    randNum = random.randint(1,100) #Generates a random number between 1 and 100
     count = 0
-    while True:
+    while True:                                                    #Runs the code till the loop breaks
         try:
             i=int(input("Guess the number between 1 and 100: "))
-            if i != randNum:
+            if i != randNum:                                        #Checking if the user input is not equal to randNum
                 diff = i-randNum
                 if diff>20:
                     print('Guess was too high, try again')
@@ -15,10 +14,10 @@ def guesser():
                     print('Guess was too low, try again')
                 count += 1
             else:
-                plural = 'ies' if count != 1 else 'y'
+                plural = 'ies' if count != 1 else 'y'              #Setting the plural values to 'ies' or 'y' to mention it while printing
                 print(f'Congratulations your guess is correct, you took {count} tr{plural}')
                 break
-        except TypeError and ValueError:
+        except ValueError:                           #This Except block runs when ValueError occurs
             print('Please input only integers')
 
 
